b792dc87dfe8cee26f907356400d0d743d490e15
Jerromy Chance <jerromy.chance31191@gmail.com>
2024-10-30 22:14:28 -0500

    Cleaned up some styles a bit and added in a new button - still workin out te design a bit

--- script.js
+++ script.js
@@ -10,32 +10,6 @@ function setVh() {
   window.addEventListener('resize', setVh);
 
 
-
-//   // Form submit message
-//   document.getElementById('contact-form').addEventListener('submit', function(event) {
-//     event.preventDefault(); // Prevent the default form submission
-
-//     // Send email to admin with form submission details
-//     emailjs.sendForm('service_j8f1nms', 'template', this)
-//         .then(function(response) {
-//             console.log('Admin email sent successfully!', response.status, response.text);
-//         }, function(error) {
-//             console.error('Failed to send admin email...', error);
-//         });
-
-//     // Send welcome email to the user
-//     emailjs.sendForm('service_j8f1nms', 'template_user', this)
-//         .then(function(response) {
-//             console.log('User welcome email sent successfully!', response.status, response.text);
-
-//             // Display confirmation message to user
-//             document.getElementById('confirmation-message').style.display = 'block';
-//         }, function(error) {
-//             console.error('Failed to send user welcome email...', error);
-//         });
-// });
-
-
   // Initialize the Calendly inline widget
   Calendly.initInlineWidget({
     url: 'https://calendly.com/norwalldesign/norwall-design-onboarding-meeting',
